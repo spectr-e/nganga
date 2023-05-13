ae05ccc6e8bf7c25d6d77998e82c13198cf45bd4
jos-the-top-dev <devs.josia@gmail.com>
2023-05-13 13:59:32 +0300

    feat: add section containers and navbar component!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,18 @@
+import { Navbar } from "./components";
+import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import "./scss/main.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
