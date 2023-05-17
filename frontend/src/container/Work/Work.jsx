32c71c4666946f889a67c652e78ffc05b2bb9073
jos-the-top-dev <devs.josia@gmail.com>
2023-05-17 18:09:56 +0300

    fix: add blur behind mobile navbar!

--- frontend/src/container/Work/Work.jsx
+++ frontend/src/container/Work/Work.jsx
@@ -1,4 +1,9 @@
 import "./Work.scss";
+import { AppWrap } from "../../wrapper";
+import { useEffect, useState } from "react";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
+import { urlFor, client } from "../../client";
 
 const Work = () => {
   return <div>Work</div>;
