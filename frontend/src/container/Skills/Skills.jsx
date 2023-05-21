452b9e84a359ada7bee7a32a13a9d2d02528b3c4
jos-the-top-dev <devs.josia@gmail.com>
2023-05-21 16:14:44 +0300

    feat: code html for experience section!

--- frontend/src/container/Skills/Skills.jsx
+++ frontend/src/container/Skills/Skills.jsx
@@ -45,9 +45,47 @@ const Skills = () => {
             </motion.div>
           ))}
         </motion.div>
+
+        <motion.div className="app__skills-exp">
+          {experiences.map((experience) => (
+            <motion.div key={experience.year} className="app__skills-exp-item">
+              {/* Year of Experience */}
+              <div className="app__skills-exp-year">
+                <p className="bold-text">{experience.year}</p>
+              </div>
+
+              {/* Works that Year */}
+              <motion.div className="app__skills-exp-works">
+                {experience.works.map((work, index) => (
+                  <>
+                    <motion.div
+                      whileInView={{ opacity: [0, 1] }}
+                      transition={{ duration: 0.5 }}
+                      className="app__skills-exp-work"
+                      data-tip
+                      data-for={work.name}
+                      key={index}
+                    >
+                      <h4 className="bold-text">{work.name}</h4>
+                      <p className="p-text">{work.company}</p>
+                    </motion.div>
+                    <ReactTooltip
+                      id={work.name}
+                      effect="solid"
+                      arrowColor="#fff"
+                      className="skills-tooltip"
+                    >
+                      {work.desc}
+                    </ReactTooltip>
+                  </>
+                ))}
+              </motion.div>
+            </motion.div>
+          ))}
+        </motion.div>
       </div>
     </>
   );
 };
 
-export default Skills;
+export default AppWrap(Skills, "skills");
