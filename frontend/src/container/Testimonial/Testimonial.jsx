fce15d7ab074bae417ec750cac8aa1ded1543b62
jos-the-top-dev <devs.josia@gmail.com>
2023-05-21 16:57:01 +0300

    feat: code testimonials html structure!

--- frontend/src/container/Testimonial/Testimonial.jsx
+++ frontend/src/container/Testimonial/Testimonial.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
-import { HiChevronRight, HiChevronLeft } from "react-icons/hi";
 import { useEffect, useState } from "react";
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 
 import { client, urlFor } from "../../client";
 import { AppWrap, MotionWrap } from "../../wrapper";
@@ -11,6 +11,9 @@ const Testimonial = () => {
   const [testimonials, setTestimonials] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  const handleClick = (index) => {
+    setCurrentIndex(index);
+  };
   useEffect(() => {
     const brandsQuery = '*[_type == "brands"]';
     const testsQuery = '*[_type == "testimonials"]';
@@ -24,11 +27,57 @@ const Testimonial = () => {
     });
   }, []);
 
-  return <div>Testimonial</div>;
+  const test = testimonials[currentIndex];
+  return (
+    <>
+      {testimonials.length && (
+        <>
+          <div className="app__testimonial app__flex">
+            <img src={urlFor(test.imageUrl)} alt="testimonials" />
+            <div className="app__testimonial-content">
+              <p className="p-text">{test.feedback}</p>
+            </div>
+            <h4 className="bold-text">{test.name}</h4>
+            <h5 className="p-text">{test.company}</h5>
+          </div>
+
+          <div className="app__testimonial-btns app__flex">
+            <div
+              className="app__flex"
+              onClick={() =>
+                handleClick(
+                  // if we are at the first index, we move to the last testimonial, else we move to the next testimonial
+                  currentIndex === 0
+                    ? testimonials.length - 1
+                    : currentIndex - 1
+                )
+              }
+            >
+              <HiChevronLeft />
+            </div>
+            <div
+              className="app__flex"
+              onClick={() =>
+                handleClick(
+                  // if we are at the last index, we move to the first testimonial, else we move to the next testimonial
+                  currentIndex === testimonials.length - 1
+                    ? 0
+                    : currentIndex + 1
+                )
+              }
+            >
+              <HiChevronRight />
+            </div>
+          </div>
+        </>
+      )}
+      <div className="app__testimonials-brands app__flex"></div>
+    </>
+  );
 };
 
 export default AppWrap(
-  MotionWrap(Testimonial, "app__testimonials"),
-  "testimonials",
+  MotionWrap(Testimonial, "app__testimonial"),
+  "testimonial",
   "app__primarybg"
 );
