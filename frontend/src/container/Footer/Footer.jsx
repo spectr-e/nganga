d17862360d0465aa2edc02b6749afd8a1b86c01b
jos-the-top-dev <devs.josia@gmail.com>
2023-05-21 17:37:43 +0300

    feat: code the contact section html structure!

--- frontend/src/container/Footer/Footer.jsx
+++ frontend/src/container/Footer/Footer.jsx
@@ -1,7 +1,107 @@
+import { useState } from "react";
+
+import { client } from "../../client";
+import { images } from "../../constants";
+import { AppWrap, MotionWrap } from "../../wrapper";
 import "./Footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { name, email, message } = formData;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const contact = {
+      _type: "contact",
+      name: name,
+      email: email,
+      message: message,
+    };
+
+    client.create(contact).then(() => {
+      setLoading(false);
+      setIsFormSubmitted(true);
+    });
+  };
+  return (
+    <>
+      <h2 className="head-text">Take a coffee & chat with me!</h2>
+      <div className="app__footer-cards">
+        <div className="app__footer-card">
+          <img src={images.email} alt="email" />
+          <a href="mailto:devs.josia@gmail.com" className="p-text">
+            devs.josia@gmail.com
+          </a>
+        </div>
+        <div className="app__footer-card">
+          <img src={images.mobile} alt="mobile" />
+          <a href="tel:+254799906167" className="p-text">
+            +254 799 906167
+          </a>
+        </div>
+      </div>
+      {!isFormSubmitted ? (
+        <div className="app__footer-form app__flex">
+          <div className="app__flex">
+            <input
+              type="text"
+              placeholder="Your Name"
+              name="name"
+              value={name}
+              onChange={handleChange}
+              className="p-text"
+            />
+            <input
+              type="email"
+              placeholder="Your Email"
+              name="eamil"
+              value={email}
+              onChange={handleChange}
+              className="p-text"
+            />
+          </div>
+          <div>
+            <textarea
+              name={message}
+              placeholder="Your Message"
+              className="p-text"
+              value={message}
+              onChange={handleChange}
+              id=""
+              cols="30"
+              rows="10"
+            />
+          </div>
+          <button className="p-text" type="button" onClick={handleSubmit}>
+            {" "}
+            {loading ? "Sending..." : "Send Message"}
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h2 className="head-text">Thank you for getting in touch!</h2>
+        </div>
+      )}
+    </>
+  );
 };
 
-export default Footer;
+export default AppWrap(
+  MotionWrap(Footer, "app__footer"),
+  "contact",
+  "app__whitebg"
+);
